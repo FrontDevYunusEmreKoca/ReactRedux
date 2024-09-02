6b634c4b79914f9dcf08cb8eaa35124d324de9c6
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-09-02 14:42:45 +0300

    dispathing actions redux

--- src/index.js
+++ src/index.js
@@ -2,32 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppRouter from './routers/AppRouter';
-import "./App.css"
+import './App.css';
+import { createStore } from 'redux';
 
-
-import {createStore} from "redux"
-
+// Initial state
 const initialState = {
-  count:5
-}
+  count: 5,
+};
 
-//store
-const store = createStore((state =  initialState) =>{
-  return state
-})
+// Reducer
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+      return {
+        ...state,  // state'in diğer özelliklerini korur
+        count: state.count + incrementBy,
+      };
+    case 'DECREMENT':
+      return {
+        ...state,  // state'in diğer özelliklerini korur
+        count: state.count - 1,
+      };
+    case 'RESET':
+      return {
+        ...state,  // state'in diğer özelliklerini korur
+        count: initialState.count,
+      };
+    default:
+      return state;
+  }
+};
 
+// Create store
+const store = createStore(counterReducer);
 
-// getState
-console.log(store.getState())
+// Subscribe to store changes
+store.subscribe(() => {
+  console.log('Current State:', store.getState());
+});
 
+// Dispatch actions
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 10,
+});
+store.dispatch({
+  type: 'DECREMENT',
+});
+store.dispatch({
+  type: 'RESET',
+});
 
-// increment , decrement ,reset => actions (object)
+console.log('Final State:', store.getState());
 
-// dispacth 
-
-// reducers
-
-
+// Render the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
