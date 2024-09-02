e3e90a7ed714c1346514c669cc55cad5e614ca85
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-09-02 14:18:15 +0300

    react redux kurulumu

--- src/index.js
+++ src/index.js
@@ -5,6 +5,29 @@ import AppRouter from './routers/AppRouter';
 import "./App.css"
 
 
+import {createStore} from "redux"
+
+const initialState = {
+  count:5
+}
+
+//store
+const store = createStore((state =  initialState) =>{
+  return state
+})
+
+
+// getState
+console.log(store.getState())
+
+
+// increment , decrement ,reset => actions (object)
+
+// dispacth 
+
+// reducers
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
